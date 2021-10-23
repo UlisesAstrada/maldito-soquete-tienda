0cd952e97eb904414a55b754291c42f1a80bd90a
Ulises <ulises_astrada@outlook.com>
2021-10-23 10:12:23 -0300

    Adding revalidate and theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import React from "react"
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, Image, Container, VStack, Heading, Text, Box, Divider} from "@chakra-ui/react"
 import { AppProps } from "next/app"
+import theme from "../theme"
 
 const App:React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Box padding={4}>
+      <Container
+      borderRadius="sm" 
+      backgroundColor="white"
+      boxShadow="medium"
+      maxWidth="container.xl"
+      padding={4}
+      >
+        <VStack marginBottom={6}>
+          <Image borderRadius={9999} alt="an image" src="//placehold.it/128x128"></Image>
+          <Heading>Maldito Soquete</Heading>
+          <Text>Tu tienda de zoquetes 🤘</Text>
+        </VStack>
+        <Divider marginY={6}/>
+        <Component {...pageProps} />
+      </Container>
+      </Box>
     </ChakraProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, Link, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Grid, Link, Stack, Text } from '@chakra-ui/react';
 import { GetStaticProps } from 'next';
 import React from 'react';
 import api from '../product/api';
@@ -8,6 +8,13 @@ interface Props{
   products: Product[];
 }
 
+function parseCurrency(value: number) : string {
+  return value.toLocaleString('es-AR', {
+    style: 'currency',
+    currency: 'ARS'
+  })
+}
+
 
 const IndexRoute: React.FC<Props> = ({products}) => {
   
@@ -15,31 +22,41 @@ const IndexRoute: React.FC<Props> = ({products}) => {
 
   const text = React.useMemo(() => {
     return cart
-    .reduce((message, product) => message.concat(`* ${product.title} - $${product.price}\n`), ``)
-    .concat(`\nTotal: ${cart.reduce((total, product) => total + product.price, 0)}`)
+    .reduce((message, product) => message.concat(`* ${product.title} - ${parseCurrency(product.price)}\n`), ``)
+    .concat(`\nTotal:
+    ${parseCurrency(cart.reduce((total, product) => total + product.price, 0))}`)
   }, [cart])
 
   return (
-    <Stack>
+    <Stack spacing={6}>
       <Grid gridGap="6" templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
     {products.map(product => (
-        <Stack backgroundColor="gray.100" key={product.id}>
+        <Stack spacing={3} borderRadius="md" padding={4} backgroundColor="gray.100" key={product.id}>
+          <Stack spacing={1}>
           <Text>{product.title}</Text>
-          <Text>{product.price}</Text>
-          <Button colorScheme="blue" onClick={() => setCart(cart => cart.concat(product))}>
+          <Text fontSize="sm" fontWeight="500" color="green.500">{parseCurrency(product.price)}</Text>
+          </Stack>
+          <Button size="sm" colorScheme="primary" variant="outline" onClick={() => setCart(cart => cart.concat(product))}>
             Agregar
           </Button>
         </Stack>
       ))}
       </Grid>
       {Boolean(cart.length) && (
-          <Button 
-            as={Link} 
-            href={`https://wa.me/5493512316539?text=${encodeURIComponent(text)}`} 
-            isExternal 
-            colorScheme="whatsapp">
-              Completar pedido ({cart.length} productos)
+        <Flex
+        alignItems="center"
+        justifyContent="center"
+        bottom={4}
+        >
+          <Button
+          width="fit-content"
+          as={Link} 
+          href={`https://wa.me/5493512316539?text=${encodeURIComponent(text)}`} 
+          isExternal
+          colorScheme="whatsapp">
+            Completar pedido ({cart.length} productos)
           </Button>
+        </Flex>
       )}
     </Stack>
   );
